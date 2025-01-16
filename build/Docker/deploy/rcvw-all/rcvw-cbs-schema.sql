2b034cb19081edbe00704bf7c115fa9fb75f8c80
GMB <31322989+gbaumgardner@users.noreply.github.com>
2025-01-16 11:20:00 -0500

    Adding CBS schema file

---
+++ build/Docker/deploy/rcvw-all/rcvw-cbs-schema.sql
@@ -0,0 +1,409 @@
+-- [rcvw-db].dbo.HRI_ACTIVATION_STATUS definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_ACTIVATION_STATUS;
+
+CREATE TABLE [rcvw-db].dbo.HRI_ACTIVATION_STATUS (
+	HRI_ID int NOT NULL,
+	FixLatitude float NULL,
+	FixLongitude float NULL,
+	PreemptionStatus bit NULL,
+	RBSOperational bit NULL,
+	ErrorCode int NULL,
+	ErrorMessage varchar(1024) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RTCMProxy bit DEFAULT 0 NULL,
+	IP varchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LastUpdated datetime DEFAULT getdate() NOT NULL,
+	CONSTRAINT PK_HRI_ACTIVATION_STATUS PRIMARY KEY (HRI_ID)
+);
+
+
+-- [rcvw-db].dbo.HRI_CROSSING_HEADER definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_CROSSING_HEADER;
+
+CREATE TABLE [rcvw-db].dbo.HRI_CROSSING_HEADER (
+	HRI_ID int NULL,
+	HRIProtWarnDevicePresent bit NULL,
+	AgencyID int NULL,
+	CountyCode nvarchar(5) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CrossingID nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	PublishedReportBaseID int NULL,
+	StateCode nvarchar(2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RailroadCode nvarchar(8) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CrossingClosed bit NULL,
+	Created datetime NULL,
+	CreatedBy nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LastUpdated datetime NULL,
+	LastUpdateBy nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_CROSSING_HEADER PRIMARY KEY (CrossingID)
+);
+
+-- [rcvw-db].dbo.HRI_HIGHWAY_TRAFFIC_CONTROL_DEVICE definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_HIGHWAY_TRAFFIC_CONTROL_DEVICE;
+
+CREATE TABLE [rcvw-db].dbo.HRI_HIGHWAY_TRAFFIC_CONTROL_DEVICE (
+	CrossingID nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	SubmissionType nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ReportBaseId int NOT NULL,
+	NoSigns nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XBuck int NULL,
+	StopStd int NULL,
+	YieldStd int NULL,
+	AdvWarn nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_1 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_2 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_3 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_4 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_11 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AdvW10_12 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Low_Grnd nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Low_GrndSigns int NULL,
+	PaveMrkIDs nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Channel nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Exempt nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	EnsSign nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	OthSgn nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	OthSgn1 int NULL,
+	OthDes1 nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	OthSgn2 int NULL,
+	OthDes2 nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	OthSgn3 int NULL,
+	OthDes3 nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	PrvxSign nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Led nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Gates int NULL,
+	GatePed int NULL,
+	GateConf nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	GateConfType nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	FlashOv int NULL,
+	FlashNov int NULL,
+	CFlashType nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	FlashPost int NULL,
+	FlashPostType nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Bkl_FlashPost nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Sdl_FlashPost nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	FlashPai int NULL,
+	AwdIDate nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AwhornChk nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	AwhornlDate nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwyTrafSignl nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Wigwags tinyint NULL,
+	Bells int NULL,
+	SpecPro nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	FlashOth int NULL,
+	FlashOthDes nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwynrSig nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Intrprmp nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	PrempType nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwtrfPsig nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwtrfPsigsdis int NULL,
+	HwtrfPsiglndis int NULL,
+	MonitorDev nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	WdCode varchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_HIGHWAY_TRAFFIC_CONTROL_DEVICE PRIMARY KEY (CrossingID),
+	CONSTRAINT FK_HRI_HIGHWAY_TRAFFIC_CONTROL_DEVICE_HRI_CROSSING_HEADER FOREIGN KEY (CrossingID) REFERENCES [rcvw-db].dbo.HRI_CROSSING_HEADER(CrossingID) ON DELETE CASCADE
+);
+
+
+-- [rcvw-db].dbo.HRI_LOCATION_AND_CLASSIFICATION definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_LOCATION_AND_CLASSIFICATION;
+
+CREATE TABLE [rcvw-db].dbo.HRI_LOCATION_AND_CLASSIFICATION (
+	CrossingID nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	SubmissionType nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ReportBaseId int NOT NULL,
+	Railroad nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StateCD nvarchar(2) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StateName nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CntyCD nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CountyName nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Nearest nvarchar(6) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CityCD nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CityName nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Street nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	BlockNumb nvarchar(6) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Highway nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SepInd nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SepRr1 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SepRr2 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SepRr3 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SepRr4 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	MultFrmsFiled nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SameInd nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SameRr1 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SameRr2 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SameRr3 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SameRr4 nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrID nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Ttstn nvarchar(6) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	TtstnNam nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrMain nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XingOwnr nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	TypeXing nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XPurpose nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	PosXing nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	OpenPub nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	TypeTrnSrvcIDs nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	DevelTypID nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XingAdj nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XngAdjNo nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Whistban nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	WhistDate nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HscoRrid nvarchar(4) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SfxHscoRrid nvarchar(4) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Latitude float NULL,
+	Longitude float NULL,
+	LLsource nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrNarr1 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrNarr2 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrNarr3 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrNarr4 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StNarr1 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StNarr2 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StNarr3 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StNarr4 nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrNarr nvarchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StNarr nvarchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	PolCont nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	RrCont nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwyCont nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_LOCATION_AND_CLASSIFICATION PRIMARY KEY (CrossingID),
+	CONSTRAINT FK_HRI_LOCATION_AND_CLASSIFICATION_HRI_CROSSING_HEADER FOREIGN KEY (CrossingID) REFERENCES [rcvw-db].dbo.HRI_CROSSING_HEADER(CrossingID)
+);
+
+
+-- [rcvw-db].dbo.HRI_PHYSICAL_CHARACTERISTICS definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_PHYSICAL_CHARACTERISTICS;
+
+CREATE TABLE [rcvw-db].dbo.HRI_PHYSICAL_CHARACTERISTICS (
+	CrossingID nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	SubmissionType nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ReportBaseId int NOT NULL,
+	TraficLn int NULL,
+	TraflnType int NULL,
+	HwyPved nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Downst nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Illumina nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XSurfDate nvarchar(6) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XSurfWidth int NULL,
+	XSurfLength int NULL,
+	XSurfaceIDs nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	XSurOthr nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwyNear nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwynDist int NULL,
+	XAngle nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ComPower nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_PHYSICAL_CHARACTERISTICS PRIMARY KEY (CrossingID),
+	CONSTRAINT FK_HRI_PHYSICAL_CHARACTERISTICS_HRI_CROSSING_HEADER FOREIGN KEY (CrossingID) REFERENCES [rcvw-db].dbo.HRI_CROSSING_HEADER(CrossingID)
+);
+
+
+-- [rcvw-db].dbo.HRI_PUBLIC_HIGHWAY definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_PUBLIC_HIGHWAY;
+
+CREATE TABLE [rcvw-db].dbo.HRI_PUBLIC_HIGHWAY (
+	CrossingID nvarchar(7) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	SubmissionType nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ReportBaseId int NOT NULL,
+	HwySys nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwyClassCD nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwyClassrdtpID nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	StHwy1 nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	HwySpeed int NULL,
+	HwySpeedps nvarchar(32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LrsRouteid nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LrsMilePost nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Aadt float NULL,
+	AadtYear nvarchar(4) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	PctTruk float NULL,
+	SchlBusChk nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SchlBsCnt float NULL,
+	HazmtVeh nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	EmrgncySrvc nvarchar(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	crossingClosed nvarchar(3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_PUBLIC_HIGHWAY PRIMARY KEY (CrossingID),
+	CONSTRAINT FK_HRI_PUBLIC_HIGHWAY_HRI_CROSSING_HEADER FOREIGN KEY (CrossingID) REFERENCES [rcvw-db].dbo.HRI_CROSSING_HEADER(CrossingID)
+);
+
+
+-- [rcvw-db].dbo.HRI_CBS_CONNECT definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.HRI_CBS_CONNECT;
+
+CREATE TABLE [rcvw-db].dbo.HRI_CBS_CONNECT (
+	HRI_ID int NOT NULL,
+	[Type] varchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Endpoint varchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Topic varchar(512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Subscription varchar(512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_HRI_CBS_CONNECT PRIMARY KEY ([Type],HRI_ID),
+	CONSTRAINT FK_HRI_CBS_CONNECT_HRI_ACTIVATION_STATUS FOREIGN KEY (HRI_ID) REFERENCES [rcvw-db].dbo.HRI_ACTIVATION_STATUS(HRI_ID)
+);
+
+
+-- [rcvw-db].dbo.RBS_INCOMING_MESSAGE definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RBS_INCOMING_MESSAGE;
+
+CREATE TABLE [rcvw-db].dbo.RBS_INCOMING_MESSAGE (
+	Message_ID uniqueidentifier NOT NULL,
+	Topic varchar(512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Subject varchar(512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Content_Type varchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Content varchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Received datetime NOT NULL,
+	Stored datetime NOT NULL,
+	CONSTRAINT PK_RBS_INCOMING_MESSAGE PRIMARY KEY (Message_ID)
+);
+ CREATE NONCLUSTERED INDEX receivedIndex ON dbo.RBS_INCOMING_MESSAGE (  Received ASC  )  
+	 WITH (  PAD_INDEX = OFF ,FILLFACTOR = 100  ,SORT_IN_TEMPDB = OFF , IGNORE_DUP_KEY = OFF , STATISTICS_NORECOMPUTE = OFF , ONLINE = OFF , ALLOW_ROW_LOCKS = ON , ALLOW_PAGE_LOCKS = ON  )
+	 ON [PRIMARY ] ;
+
+
+-- [rcvw-db].dbo.RBS_IEEE1570_4909_MESSAGE definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RBS_IEEE1570_4909_MESSAGE;
+
+CREATE TABLE [rcvw-db].dbo.RBS_IEEE1570_4909_MESSAGE (
+	Message_ID uniqueidentifier NOT NULL,
+	HRI_ID int NOT NULL,
+	IEEE1570 bit NOT NULL,
+	xingSequence int NULL,
+	xingNumTracks int NULL,
+	xingNumTrains bit NULL,
+	xingOperational bit NULL,
+	xingTrainDetected bit NULL,
+	xingWarningActive bit NULL,
+	xingPreemptionActive bit NULL,
+	xingEgressGates bit NULL,
+	xingEgressGatesUp bit NULL,
+	xingEgressGatesDown bit NULL,
+	xingIngressGates bit NULL,
+	xingIngressGatesUp bit NULL,
+	xingIngressGatesDown bit NULL,
+	xingPreemptionDesignSeconds int NULL,
+	trainSequence int NULL,
+	trainDirection text COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	trainIslandOccupied bit NULL,
+	trainWarningDesignTime int NULL,
+	trainEstimatedWarningSeconds int NULL,
+	CONSTRAINT FK_RBS_IEEE1509_4909_MESSAGE_RBS_INCOMING_MESSAGE FOREIGN KEY (Message_ID) REFERENCES [rcvw-db].dbo.RBS_INCOMING_MESSAGE(Message_ID),
+	CONSTRAINT FK_RBS_INCOMING_1509_CROSSINGS_HRI_ACTIVATION_STATUS FOREIGN KEY (HRI_ID) REFERENCES [rcvw-db].dbo.HRI_ACTIVATION_STATUS(HRI_ID)
+);
+
+
+-- [rcvw-db].dbo.RBS_INCOMING_MAP definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RBS_INCOMING_MAP;
+
+CREATE TABLE [rcvw-db].dbo.RBS_INCOMING_MAP (
+	Message_ID uniqueidentifier NOT NULL,
+	HRI_ID int NOT NULL,
+	TrackedLane int NOT NULL,
+	SignalGroup int NOT NULL,
+	RefPointLatitude float NOT NULL,
+	RefPointLongitude float NOT NULL,
+	Received datetime DEFAULT getdate() NULL,
+	CONSTRAINT FK_RBS_INCOMING_MAP_HRI_ACTIVATION_STATUS FOREIGN KEY (HRI_ID) REFERENCES [rcvw-db].dbo.HRI_ACTIVATION_STATUS(HRI_ID),
+	CONSTRAINT FK_RBS_INCOMING_MAP_RBS_INCOMING_MESSAGE FOREIGN KEY (Message_ID) REFERENCES [rcvw-db].dbo.RBS_INCOMING_MESSAGE(Message_ID)
+);
+ CREATE NONCLUSTERED INDEX nci_msft_1_RBS_INCOMING_MAP_2F7F6C58CFE38F9CDA79D649EC2F9189 ON dbo.RBS_INCOMING_MAP (  HRI_ID ASC  , Received ASC  )  
+	 INCLUDE ( SignalGroup ) 
+	 WITH (  PAD_INDEX = OFF ,FILLFACTOR = 100  ,SORT_IN_TEMPDB = OFF , IGNORE_DUP_KEY = OFF , STATISTICS_NORECOMPUTE = OFF , ONLINE = OFF , ALLOW_ROW_LOCKS = ON , ALLOW_PAGE_LOCKS = ON  )
+	 ON [PRIMARY ] ;
+
+
+-- [rcvw-db].dbo.RBS_INCOMING_SPAT definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RBS_INCOMING_SPAT;
+
+CREATE TABLE [rcvw-db].dbo.RBS_INCOMING_SPAT (
+	Message_ID uniqueidentifier NOT NULL,
+	HRI_ID int NOT NULL,
+	IntersectionName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	MsgTime datetime NOT NULL,
+	ActiveSignalGroup int NOT NULL,
+	CONSTRAINT FK_RBS_INCOMING_SPAT_HRI_ACTIVATION_STATUS FOREIGN KEY (HRI_ID) REFERENCES [rcvw-db].dbo.HRI_ACTIVATION_STATUS(HRI_ID),
+	CONSTRAINT FK_RBS_INCOMING_SPAT_RBS_INCOMING_MESSAGE FOREIGN KEY (Message_ID) REFERENCES [rcvw-db].dbo.RBS_INCOMING_MESSAGE(Message_ID)
+);
+ CREATE NONCLUSTERED INDEX Index_RBS_INCOMING_SPAT_1 ON dbo.RBS_INCOMING_SPAT (  HRI_ID ASC  , MsgTime ASC  )  
+	 WITH (  PAD_INDEX = OFF ,FILLFACTOR = 100  ,SORT_IN_TEMPDB = OFF , IGNORE_DUP_KEY = OFF , STATISTICS_NORECOMPUTE = OFF , ONLINE = OFF , ALLOW_ROW_LOCKS = ON , ALLOW_PAGE_LOCKS = ON  )
+	 ON [PRIMARY ] ;
+
+-- [rcvw-db].dbo.RBS_RADIOS definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RBS_RADIOS;
+
+CREATE TABLE [rcvw-db].dbo.RBS_RADIOS (
+	HRI_ID int NOT NULL,
+	Make varchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Version varchar(1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Location varchar(1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Firmware varchar(1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Power int NOT NULL,
+	Mode varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Sent int NOT NULL,
+	Received int NOT NULL
+);
+
+
+-- [rcvw-db].dbo.RCVW_CLIENTS definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RCVW_CLIENTS;
+
+CREATE TABLE [rcvw-db].dbo.RCVW_CLIENTS (
+	UUID varchar(64) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	ENTITY_NAME varchar(64) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	LAST_QUERY_DATE datetime DEFAULT getdate() NOT NULL,
+	CONSTRAINT PK__RCVW_CLI__65A475E7D471BCC1 PRIMARY KEY (UUID)
+);
+
+-- [rcvw-db].dbo.RSU_GPS_OUTPUT definition
+
+-- Drop table
+
+-- DROP TABLE [rcvw-db].dbo.RSU_GPS_OUTPUT;
+
+CREATE TABLE [rcvw-db].dbo.RSU_GPS_OUTPUT (
+	HRI_ID int NOT NULL,
+	rsuGpsOutputPort int NULL,
+	rsuGpsOutputAddress varchar(40) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	rsuOutputInterface varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	rsuGpsOutputInterval int NULL,
+	rsuGpsOutputString varchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	rsuGpsRefLat int NULL,
+	rsuGpsRefLon int NULL,
+	rsuGpsRefElv int NULL,
+	rsuGpsMaxDeviation int NULL,
+	CONSTRAINT FK_RSU_GPS_OUTPUT_HRI_ACTIVATION_STATUS FOREIGN KEY (HRI_ID) REFERENCES [rcvw-db].dbo.HRI_ACTIVATION_STATUS(HRI_ID)
+);
